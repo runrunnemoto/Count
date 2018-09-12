d02bd1e1ac34e631e7bd7e726ff2e3e6b8f16df4
Kakeru Nemoto <drumgadaisuki@gmail.com>
2018-09-12 15:28:21 +0900

    ラベルずらしました。

--- Count/ViewController.swift
+++ Count/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var number: Int = 0
+    @IBOutlet var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +23,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func plus() {
+        number = number + 1
+        label.text = String(number)
+        
+        if number <= -1 {
+            label.textColor = UIColor.blue
+        }else{
+            label.textColor = UIColor.red
+        }
+    }
+    
+    @IBAction func minus() {
+        number = number - 1
+        label.text = String(number)
+        
+        if number <= -1 {
+            label.textColor = UIColor.blue
+        }else{
+            label.textColor = UIColor.red
+        }
+    }
+    
+    
 }
 
